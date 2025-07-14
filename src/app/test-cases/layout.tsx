cce5b819bddc409025695070678bd49a4a7c2cd4
Eynar92 <eynar.j.alvarez.m@gmail.com>
2025-07-13 20:33:27 -0400

    Add dynamic page and update mock data and grid main page

---
+++ src/app/test-cases/layout.tsx
@@ -0,0 +1,22 @@
+import { AddTestCaseDialog } from "@/components/AddTestCaseDialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function TestCasesLayout({
+    children
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <main className="px-4 py-4 sm:px-8 lg:px-16 space-y-4">
+            <header className="flex justify-between">
+                <Input
+                    placeholder="Search..."
+                    className="max-w-1/2"
+                />
+                <AddTestCaseDialog />
+            </header>
+            {children}
+        </main>
+    );
+}
